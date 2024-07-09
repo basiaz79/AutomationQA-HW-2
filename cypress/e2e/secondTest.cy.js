f1125dbe6354737cdaba6dfbab075c4f457dd03d
basiaz79 <bzasowski79@gmail.com>
2024-07-09 13:28:30 +0200

    Automation HW 2

---
+++ cypress/e2e/secondTest.cy.js
@@ -0,0 +1,59 @@
+describe("User can visit GoIT page", () => {
+    beforeEach("Go to page", () => {
+        cy.visit("https://www.edu.goit.global/account/login")
+    });
+    
+    it("First Test log in and out", () => {
+        cy.get('#user_email').type("user888@gmail.com");
+        cy.get('input[type="password"]').type("1234567890");
+        cy.get('button[type="submit"]').click();
+    });
+});
+
+  
+
+
+  describe('Login Test 1', () => {
+    beforeEach("Go to page", () => {
+        cy.visit("https://www.edu.goit.global/account/login")
+    });
+
+    it('Test 1 Login and Logout', () => {
+        cy.get('#user_email').type("user888@gmail.com");
+        cy.get('input[type="password"]').type("1234567890");
+        cy.get('button[type="submit"]').click();
+  
+      // Znajdź przycisk w prawym górnym rogu i kliknij na niego, aby otworzyć menu
+        cy.get('.header-right .menu-button').click();
+  
+      // Znajdź przycisk Log out w menu i kliknij na niego
+        cy.contains('Log out').click();
+  
+      // Upewnij się, że wróciłeś do strony logowania
+        cy.url().should('include', '/account/login');
+    });
+  });
+
+  
+  describe('Login Test 2', () => {
+    beforeEach("Go to page", () => {
+        cy.visit("https://www.edu.goit.global/account/login")
+    });
+
+    it('Test 2: Login and Logout with different credentials', () => {
+        cy.get('#user_email').type("testowyqa@qa.team");
+        cy.get('input[type="password"]').type("QA!automation-1");
+        cy.get('button[type="submit"]').click();
+        
+  
+      // Znajdź przycisk w prawym górnym rogu i kliknij na niego, aby otworzyć menu
+        cy.get('.header-right .menu-button').click();
+  
+      // Znajdź przycisk Log out w menu i kliknij na niego
+        cy.contains('Log out').click();
+  
+      // Upewnij się, że wróciłeś do strony logowania
+        cy.url().should('include', '/account/login');
+    });
+  });
+  
