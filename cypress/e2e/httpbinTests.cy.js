4f263649fb93eeaf59e1ba8c2e05355d525cb1f2
basiaz79 <bzasowski79@gmail.com>
2024-07-15 17:37:41 +0200

    HW 4

---
+++ cypress/e2e/httpbinTests.cy.js
@@ -0,0 +1,10 @@
+//Get and check Code response is 200
+describe('HTTPBin API Tests', () => {
+    it('response code should be 200', () => {
+        cy.request('https://httpbin.org').then(response => {
+          const status = response.status;
+    
+          assert.equal(200, status);
+        })
+      })
+    }) 
